a2b59bdfc544a219cd0a876c79fcb7693719d78b
Gabi <xgceldia796@ieshnosmachado.org>
2025-12-05 12:49:06 +0100

    borrado agregando gitignore correcto.

--- addons/library/models/editorial.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class editorial(models.Model):
-    _name = 'library.editorial'
-    _description = 'Editorial'
-
-    name = fields.Char("Editorial", size=64, required=True)
-    book_ids = fields.One2many('library.book', 'editorial_id', string="Libros")
